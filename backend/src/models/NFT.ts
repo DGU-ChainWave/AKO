e23fe64d09b1627b732546423aabb9ffa76933cc
Kong-E <kng001016@dgu.ac.kr>
2024-02-19 13:49:39 +0900

    fix: mod models

--- backend/src/models/NFT.ts
+++ backend/src/models/NFT.ts
@@ -2,18 +2,14 @@ import mongoose from 'mongoose';
 
 const NFTSchema = new mongoose.Schema(
   {
-    tokenId: {
-      type: Number,
+    imageUri: {
+      type: String,
       required: true,
       unique: true,
     },
     owner: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
-    },
-    voteCount: {
-      type: Number,
-      default: 0,
     },
   },
   { timestamps: true },
--- backend/src/models/Vote.ts
+++ backend/src/models/Vote.ts
@@ -2,16 +2,19 @@ import mongoose from 'mongoose';
 
 const VoteSchema = new mongoose.Schema(
   {
-    nftId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'NFT',
+    imageUri: {
+      type: String,
+      required: true,
+      unique: true,
     },
     voter: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
+      // 투표한 사람 배열
+      default: [] as string[],
     },
   },
-  { timestamps: true }, 
+  { timestamps: true },
 );
 
 const Vote = mongoose.model('Vote', VoteSchema);
