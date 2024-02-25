3f743a05a892a2974311072b273802fead6eca72
Kong-E <kng001016@dgu.ac.kr>
2024-02-26 00:05:59 +0900

    fix: Header css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,22 @@
-import React, { useState } from "react";
-import MenuModal from "./Modal/MenuModal";
+import React, { useState } from 'react';
+import MenuModal from './Modal/MenuModal';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-    const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
-    const [isMenuModalOpen, setIsMenuModalOpen] = useState(false);
-    const clickInfo = () => setIsInfoModalOpen(!isInfoModalOpen);
-    const clickMenu = () => setIsMenuModalOpen(!isMenuModalOpen);
+  const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
+  const [isMenuModalOpen, setIsMenuModalOpen] = useState(false);
+  const clickInfo = () => setIsInfoModalOpen(!isInfoModalOpen);
+  const clickMenu = () => setIsMenuModalOpen(!isMenuModalOpen);
 
-    return(
-        <div className="flex justify-center">
-            <div className="absolute top-3.5 flex items-center w-[363px] h-[39px] z-40">
-        <a href="/main" aria-label="go to main" className="absolute left-0">
-          <img src="Logo.png" alt="logo"/>
-        </a>
-        <button onClick={clickMenu} className="absolute right-0 flex justify-center">
-          <img src="list.svg" alt="list icon" />
-        </button>
-        {isMenuModalOpen && <MenuModal onClose={setIsMenuModalOpen} />}
-      </div>
-        </div>
-    );
+  return (
+    <div className="relative mx-auto max-w-[430px] flex justify-between items-center w-screen z-40 p-3">
+      <Link to="/main" aria-label="go to main" className="left-0">
+        <img src="Logo.png" alt="logo" className="w-[86px]" />
+      </Link>
+      <button onClick={clickMenu} className="right-0 flex justify-center pr-1">
+        <img src="list.svg" alt="list icon" />
+      </button>
+      {isMenuModalOpen && <MenuModal onClose={setIsMenuModalOpen} />}
+    </div>
+  );
 }
