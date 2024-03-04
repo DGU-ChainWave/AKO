c2b66194850378de82f0d89a63d3d1837ef6b577
mchaeeun <chaeeun@dgu.ac.kr>
2024-03-04 23:23:30 +0900

    feat: add intro 3d AKO model

--- frontend/src/pages/Intro/Intro.jsx
+++ frontend/src/pages/Intro/Intro.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import Model from '../../assets/images/Ppoo';
 
 export default function Intro() {
-  return (
+  return (  
     <Link to="/main">
       <div className="max-w-[430px] mx-auto flex flex-col gap-[30px] justify-center items-center w-screen h-svh bg-gradient-to-b from-[#FECD93] to-[#FFA234]">
         <div className="flex gap-2 items-center pt-[15px]">
@@ -17,7 +20,14 @@ export default function Intro() {
           <p className="text-[20px] font-bold text-white animate-pulse pt-[20px]">
             화면을 터치하세요!
           </p>
-          <img src="assets/ako_3d.png" alt="ako" className="w-[280px]" />
+          {/* <img src="assets/ako_3d.png" alt="ako" className="w-[280px]" />*/}
+          <Canvas style={{height: '50vh' }}>
+            <OrbitControls autoRotate={true}/>
+            <ambientLight intensity={7} />
+            <group scale={[1.5,1.5,1.5]} rotation-y={-Math.PI / 2}>
+              <Model />
+            </group>
+          </Canvas>
           {/* <div className="absolute top-[448px] w-[297px] h-[136px] bg-[#D9D9D9]"></div> */}
         </div>
       </div>
